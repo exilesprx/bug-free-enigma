3d9cf49844697d02a1838b1c26497ef687c87ea3
Exiles.prx <exiles.prx@gmail.com>
2024-03-01 23:05:57 -0500

    Solved problem 67

--- exercises/067_comptime2.zig
+++ exercises/067_comptime2.zig
@@ -35,7 +35,7 @@ pub fn main() void {
     // In this contrived example, we've decided to allocate some
     // arrays using a variable count! But something's missing...
     //
-    var count = 0;
+    comptime var count = 0;
 
     count += 1;
     const a1: [count]u8 = .{'A'} ** count;
@@ -60,5 +60,5 @@ pub fn main() void {
     //
     // Try uncommenting this line and playing around with it
     // (copy it, move it) to see what it does:
-    //@compileLog("Count at compile time: ", count);
+    // @compileLog("Count at compile time: ", count);
 }
